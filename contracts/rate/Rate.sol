29c1f2a480a805dedf664482a1474f5492d810ed
Ali AbdolAzimi <A.AbdolAzimi@parlar.com>
2023-11-30 16:52:55 +0330

    add EIP712 base

--- contracts/rate/Rate.sol
+++ contracts/rate/Rate.sol
@@ -12,7 +12,10 @@ contract Rate is IRate {
         string info;
         bool exists;
     }
-
+    bytes32 public constant ADD_SERVICE_TYPE_HASH =
+        keccak256(
+            "addService(uint256 nonce,string infoHash,string serviceAddress)"
+        );
     mapping(address => Service) public services;
 
     constructor(address _accessRestrictionAddress) {
@@ -29,10 +32,31 @@ contract Rate is IRate {
     }
 
     function addService(
+        uint256 _nonce,
+        address _submitter,
         address _service,
-        string calldata _infoHash,
-        address _submitter
+        string memory _infoHash,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
     ) external isScriptOnly(msg.sender) {
+        _checkSigner(
+            _buildDomainSeparator(),
+            keccak256(
+                abi.encode(
+                    ADD_SERVICE_TYPE_HASH,
+                    _nonce,
+                    keccak256(bytes(_infoHash)),
+                    _service,
+
+                )
+            ),
+            _submitter,
+            _v,
+            _r,
+            _s
+        );
+
         Service storage serviceData = services[_service];
         require(
             serviceData.submiter == address(0),
@@ -43,5 +67,51 @@ contract Rate is IRate {
         serviceData.exists = true;
 
         emit ServiceAdded(_service, _submiter, _infoHash);
+    }
+
+    function _toTypedDataHash(
+        bytes32 _domainSeperator,
+        bytes32 _structHash
+    ) private pure returns (bytes32) {
+        return
+            keccak256(
+                abi.encodePacked("\x19\x01", _domainSeperator, _structHash)
+            );
+    }
+
+    /**
+     * @dev check if the given planter is the signer of given signature or not
+     */
+    function _checkSigner(
+        bytes32 _domainSeparator,
+        bytes32 _hashStruct,
+        address _signer,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) private pure {
+        bytes32 hash = _toTypedDataHash(_domainSeparator, _hashStruct);
+
+        address signer = ecrecover(hash, _v, _r, _s);
+
+        require(signer == _signer, "invalid signature");
+    }
+
+    /**
+     * @dev return domain separator
+     */
+    function _buildDomainSeparator() private view returns (bytes32) {
+        return
+            keccak256(
+                abi.encode(
+                    keccak256(
+                        "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+                    ),
+                    keccak256(bytes("Derate Protocol")),
+                    keccak256(bytes("1")),
+                    block.chainid,
+                    address(this)
+                )
+            );
     }
 }
