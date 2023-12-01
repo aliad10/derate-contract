a24db9d163f76dcf7996e471bd6a6f7dc3a779bf
abdolazim35@yahoo.com <abdolazim35@yahoo.com>
2023-12-01 22:37:23 +0330

    add feedback funcs

--- contracts/rate/IRate.sol
+++ contracts/rate/IRate.sol
@@ -3,4 +3,36 @@ pragma solidity ^0.8.20;
 
 interface IRate {
     event ServiceAdded(address service, address submiter, string info);
+    event FeedbackSubmited(address service, address submiter, string info);
+
+    function addService(
+        uint256 _nonce,
+        address _submiter,
+        address _service,
+        string memory _infoHash,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) external;
+
+    function submitFeedbackToService(
+        uint256 _nonce,
+        address _submiter,
+        address _service,
+        string memory _infoHash,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) external;
+
+    function submitFeedbackToFeedback(
+        uint256 _nonce,
+        address _prevSubmiter,
+        address _submiter,
+        address _service,
+        string memory _infoHash,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) external;
 }
--- contracts/rate/Rate.sol
+++ contracts/rate/Rate.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 // import IAccessRestriction from "./../accessRestriction/IAccessRestriction.sol";
 import "./IRate.sol";
-import "./IAccessRestriction.sol";
+import "./../accessRestriction/IAccessRestriction.sol";
 
 contract Rate is IRate {
     IAccessRestriction public accessRestriction;
@@ -12,13 +12,35 @@ contract Rate is IRate {
         string info;
         bool exists;
     }
+
+    struct Feedback {
+        string info;
+        bool exists;
+    }
     bytes32 public constant ADD_SERVICE_TYPE_HASH =
         keccak256(
             "addService(uint256 nonce,string infoHash,string serviceAddress)"
         );
-    mapping(address => Service) public services;
 
-    constructor(address _accessRestrictionAddress) {
+    bytes32 public constant FEEDBACK_TYPE_HASH =
+        keccak256(
+            "feedbackToService(uint256 nonce,string infoHash,string serviceAddress)"
+        );
+    bytes32 public constant FEEDBACK_ON_FEEDBACK_TYPE_HASH =
+        keccak256(
+            "feedbackToFeedback(uint256 nonce,string infoHash,string prevSubmiter,string serviceAddress)"
+        );
+
+    mapping(address => Service) public services;
+    mapping(address => mapping(address => Feedback)) public serviceFeedbacks; //submiter->service->data
+    mapping(address => mapping(address => mapping(address => Feedback)))
+        public feedbackFeedbacks; // submiter->prevSubmiter->service->data
+
+    mapping(address => uint256) public serviceNonce;
+    mapping(address => uint256) public feedbackNonce;
+    mapping(address => uint256) public feedbackOnfeedbackNonce;
+
+    constructor(address _address) {
         IAccessRestriction accessRestriction = IAccessRestriction(_address);
     }
 
@@ -33,13 +55,15 @@ contract Rate is IRate {
 
     function addService(
         uint256 _nonce,
-        address _submitter,
+        address _submiter,
         address _service,
         string memory _infoHash,
         uint8 _v,
         bytes32 _r,
         bytes32 _s
-    ) external isScriptOnly(msg.sender) {
+    ) external override isScriptOnly(msg.sender) {
+        require(serviceNonce[_submiter] < _nonce, "nonce is incorrect");
+
         _checkSigner(
             _buildDomainSeparator(),
             keccak256(
@@ -47,26 +71,104 @@ contract Rate is IRate {
                     ADD_SERVICE_TYPE_HASH,
                     _nonce,
                     keccak256(bytes(_infoHash)),
-                    _service,
-
+                    _service
                 )
             ),
-            _submitter,
+            _submiter,
             _v,
             _r,
             _s
         );
 
         Service storage serviceData = services[_service];
-        require(
-            serviceData.submiter == address(0),
-            "service already submitted"
-        );
+        require(serviceData.exists == false, "service already submited");
         serviceData.info = _infoHash;
         serviceData.submiter = _submiter;
         serviceData.exists = true;
 
+        serviceNonce[_submiter] = _nonce;
+
         emit ServiceAdded(_service, _submiter, _infoHash);
+    }
+
+    function submitFeedbackToService(
+        uint256 _nonce,
+        address _submiter,
+        address _service,
+        string memory _infoHash,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) external override isScriptOnly(msg.sender) {
+        require(feedbackNonce[_submiter] < _nonce, "nonce is incorrect");
+        _checkSigner(
+            _buildDomainSeparator(),
+            keccak256(
+                abi.encode(
+                    FEEDBACK_TYPE_HASH,
+                    _nonce,
+                    keccak256(bytes(_infoHash)),
+                    _service
+                )
+            ),
+            _submiter,
+            _v,
+            _r,
+            _s
+        );
+
+        Feedback storage feedbackData = serviceFeedbacks[_submiter][_service];
+
+        require(feedbackData.exists == false, "feedback already submited");
+        feedbackData.info = _infoHash;
+        feedbackData.exists = true;
+
+        feedbackNonce[_submiter] = _nonce;
+
+        emit FeedbackSubmited(_service, _submiter, _infoHash);
+    }
+
+    function submitFeedbackToFeedback(
+        uint256 _nonce,
+        address _prevSubmiter,
+        address _submiter,
+        address _service,
+        string memory _infoHash,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) external override isScriptOnly(msg.sender) {
+        require(
+            feedbackOnfeedbackNonce[_submiter] < _nonce,
+            "nonce is incorrect"
+        );
+
+        _checkSigner(
+            _buildDomainSeparator(),
+            keccak256(
+                abi.encode(
+                    FEEDBACK_ON_FEEDBACK_TYPE_HASH,
+                    _nonce,
+                    keccak256(bytes(_infoHash)),
+                    _prevSubmiter,
+                    _service
+                )
+            ),
+            _submiter,
+            _v,
+            _r,
+            _s
+        );
+
+        Feedback storage feedbackData = feedbackFeedbacks[_submiter][
+            _prevSubmiter
+        ][_service];
+
+        require(feedbackData.exists == false, "feedback already submited");
+        feedbackData.info = _infoHash;
+        feedbackData.exists = true;
+
+        feedbackOnfeedbackNonce[_submiter] = _nonce;
     }
 
     function _toTypedDataHash(
